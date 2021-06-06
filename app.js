d72931f9bcf465c12636f54f2025f6befbb7fe65
Pursuit <rahin6810@gmail.com>
2021-06-06 03:25:46 -0400

    added seperate route to process and return data from db

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const { raffleApp } = require("./routes/raffles");
+const { dataApp } = require("./routes/data");
 
 app.use("/raffles", raffleApp);
+app.use("/data", dataApp);
 
 module.exports = app;
